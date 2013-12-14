a6b4ab60ad2f71225beabdaf824c05f8e5c04ac2
Graham Holden <graham@opensussex.net>
2013-12-14 16:30:39 +0000

    reshuffle files

--- wark/views/footer.view.php
+++
@@ -1,9 +0,0 @@
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js" ></script>
-<?php
-if(isset($view_vars['js_view'])){
-	echo $view_vars['js_view']; 
-}
-?>
-</div>
-</body>
-</html>
