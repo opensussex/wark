fcf925af8b50f2d324d5c152a5cda20edde68659
Graham Holden <graham@opensussex.net>
2013-12-14 21:02:56 +0000

    build in setNames, setTable and getColumns in core Model

--- core/Model.class.php
+++ core/Model.class.php
@@ -6,6 +6,7 @@
  
  	class Model{
  		private $db = null;
+ 		private $table = null;
 		public $db_handle = null;
  		public function __construct($db = null){
  			$this->db = $db;
@@ -19,6 +20,28 @@
 		
 		public function getDBHandle(){
 			return $this->db_handle;
+		}
+
+		public function setTable($table){
+			$this->table = $table;
+			echo $this->table;
+		}
+
+		public function setNames($ser_names){
+			$this->db_handle->exec($this->set_names);
+		}
+
+		public function getColumns(){
+
+			$sql = "SELECT * FROM $this->table LIMIT 0";
+			$st = $this->db_handle->prepare($sql);
+			$st->execute();
+			for ($i = 0; $i < $st->columnCount(); $i++) {
+			    $col = $st->getColumnMeta($i);
+			    $columns[] = $col['name'];
+			}
+			print_r($columns);
+			
 		}
  	}
 ?>
--- deploy/models/ExampleModel.class.php
+++ deploy/models/ExampleModel.class.php
@@ -3,7 +3,12 @@
 		
 		private $table = 'example';
 		private $set_names = "SET NAMES utf8";
-		
+
+		public function __construct($db = null){
+			parent::__construct($db);
+			parent::setTable($this->table);
+			parent::setNames($this->set_names);
+		}
 						
 		public function example(){
 		
