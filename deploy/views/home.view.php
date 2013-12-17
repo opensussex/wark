1904d536b53d3a712cc9cee3d8a2d00ab6e854c3
Graham Holden <graham@opensussex.net>
2013-12-17 21:19:31 +0000

    change the deploy view

--- deploy/views/home.view.php
+++ deploy/views/home.view.php
@@ -1,21 +1,5 @@
-<h1>Bitcrypt</h1>
+<h1>Home</h1>
 
-<h2>Registration Form</h2>
-
-<?php
-	if(isset($view_vars['message'])){
-?>
-		<p class="message"><?=$view_vars['message'];?></p>
-<?php
-	}
-?>
-<form name="register" action="index.php" method="post">
-	<label>Email</label>
-	<input type="text" name="email" id="email" style="display-inline" />
-
-	<label>Password</label>
-	<input type="password" name="password1" />
-	<label>Confirm Password</label>
-	<input type="password" name="password2" />
-	<input type="submit" value="Register" />
-</form>
+<p>
+an example of a view.
+</p>
