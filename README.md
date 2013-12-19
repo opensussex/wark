f4740b717df03e172f085cea151d43dc0f72cb20
Graham Holden <graham@opensussex.net>
2013-12-19 20:25:48 +0000

    add into boot.php all the stuff that gets things working!

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ wark
 ====
 
 lightweight mvc
+
+We have a dependency on http://www.redbeanphp.com as we want to use this as an ORM.
--- core/boot.php
+++ core/boot.php
@@ -1,7 +1,34 @@
 <?php
 
-include_once(CORE_DIR."Controller.class.php");
-include_once(CORE_DIR."DbConnector.class.php");
-include_once(CORE_DIR."Model.class.php");
+	include_once(CORE_DIR."Controller.class.php");
+	include_once(CORE_DIR."DbConnector.class.php");
+	include_once(CORE_DIR."Model.class.php");
+
+	if(defined('THIRD_PARTY_LIBS')){
+		foreach(unserialize(THIRD_PARTY_LIBS) as $lib){
+			include_once($lib);
+		}
+	}
+
+	if(isset($_GET['route'])){ 
+		$route = $_GET['route'];
+		$controller = str_replace('.html','',$route);
+	}else{
+		$controller = 'home'; // this is our default view if nothing is passed.
+	}	
+	
+	
+	//Load the controller
+	$controller_file = DEPLOY_DIR . CONTROLLER_DIR.ucfirst($controller).CONTROLLER_SUFFIX;
+	
+	if(!file_exists($controller_file)){ // check to see if the controller file exists, if it doesn't then we load the 404 page.
+		$controller_file = DEPLOY_DIR . CONTROLLER_DIR.'Fourofour'.CONTROLLER_SUFFIX;
+		$controller = 'Fourofour';
+	}
+	
+	$db = new DbConnector();
+	include_once($controller_file);
+	$ctrlObj = new $controller($db);
+	$ctrlObj->index();
 
 ?>
