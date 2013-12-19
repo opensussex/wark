16f49b886cc5a57d075c8942d47dd24328fcfdf1
Graham Holden <graham@opensussex.net>
2013-12-19 20:34:09 +0000

    add in the boot a mechanism to load models if we're using RedBeanPHP

--- core/boot.php
+++ core/boot.php
@@ -10,14 +10,29 @@
 		}
 	}
 
+	if(defined('USE_RBPHP')){
+		if(USE_RBPHP){
+			// we want to use RedBeanPHP as our ORM as such we want to see if we need to load some models.
+			
+			if ($handle = opendir(DEPLOY_DIR . MODEL_DIR)) { // check the directory
+			    while (false !== ($entry = readdir($handle))) { // loop through the models and load them
+			        if ($entry != "." && $entry != "..") {
+						include_once($lib);	            
+			        }
+			    }
+
+			    closedir($handle);
+			}
+		}
+	}
+
 	if(isset($_GET['route'])){ 
 		$route = $_GET['route'];
 		$controller = str_replace('.html','',$route);
 	}else{
 		$controller = 'home'; // this is our default view if nothing is passed.
 	}	
-	
-	
+
 	//Load the controller
 	$controller_file = DEPLOY_DIR . CONTROLLER_DIR.ucfirst($controller).CONTROLLER_SUFFIX;
 	
