fc848a7ef046d8a658d713652450d2c1ba33f4ab
Graham Holden <opensussex@gmail.com>
2015-07-30 15:38:59 +0100

    add in a Wark class - the application entry point

---
+++ wark_core/Wark.class.php
@@ -0,0 +1,60 @@
+<?php 
+
+    class Wark
+    {
+
+        private $db;
+        public function __construct($db)
+        {
+            $this->db = $db;
+        }
+
+        public function go($route)
+        {
+
+            $method = null;
+            $method_val = null;
+            if($route){ 
+                $route = explode('/',$_GET['route']);
+                $controller = $route[0];
+                if(isset($route[1])){
+                    $method = $route[1];
+                }
+
+                if(isset($route[2])){
+                    $method_val = $route[2];
+                }
+            }else{
+                if(defined('DEFAULT_CONTROLLER')){
+                    $controller = DEFAULT_CONTROLLER;
+                }else{
+                    $controller = 'home'; // this is our default view if nothing is passed. 
+                }
+                
+            }    
+            //Load the controller
+            $controller_file = DEPLOY_DIR . CONTROLLER_DIR.ucfirst($controller).CONTROLLER_SUFFIX;
+            
+            if(!file_exists($controller_file)){ // check to see if the controller file exists, if it doesn't then we load the 404 page.
+                $controller_file = DEPLOY_DIR . CONTROLLER_DIR.'Fourofour'.CONTROLLER_SUFFIX;
+                $controller = 'Fourofour';
+            }
+            
+            
+            include_once($controller_file);
+            $ctrlObj = new $controller($this->db);
+
+
+            if($method){
+                if(method_exists($ctrlObj,$method)){
+                    $ctrlObj->$method($method_val);
+                    exit();
+                }else{
+                    $ctrlObj->index();
+                }
+                
+            }else{
+                $ctrlObj->index($method_val);   
+            }            
+        }
+    }
--- wark_core/boot.php
+++ wark_core/boot.php
@@ -28,50 +28,11 @@
 	}
 
 
-	$method = null;
-	$method_val = null;
-
-	if(isset($_GET['route'])){ 
-		$route = explode('/',$_GET['route']);
-		$controller = $route[0];
-		if(isset($route[1])){
-			$method = $route[1];
-		}
-
-		if(isset($route[2])){
-			$method_val = $route[2];
-		}
-	}else{
-		if(defined('DEFAULT_CONTROLLER')){
-			$controller = DEFAULT_CONTROLLER;
-		}else{
-			$controller = 'home'; // this is our default view if nothing is passed.	
-		}
-		
-	}	
-
-	//Load the controller
-	$controller_file = DEPLOY_DIR . CONTROLLER_DIR.ucfirst($controller).CONTROLLER_SUFFIX;
-	
-	if(!file_exists($controller_file)){ // check to see if the controller file exists, if it doesn't then we load the 404 page.
-		$controller_file = DEPLOY_DIR . CONTROLLER_DIR.'Fourofour'.CONTROLLER_SUFFIX;
-		$controller = 'Fourofour';
+	$route = null;
+	if (isset($_GET['route'])) {
+		$route = $_GET['route'];
 	}
-	
 
-	$db = new DbConnector();	
-	include_once($controller_file);
-	$ctrlObj = new $controller($db);
-
-
-	if($method){
-		if(method_exists($ctrlObj,$method)){
-			$ctrlObj->$method($method_val);
-			exit();
-		}else{
-			$ctrlObj->index();
-		}
-		
-	}else{
-		$ctrlObj->index($method_val);	
-	}
+	$db = new DbConnector();
+	$wark = new Wark($db);
+	$wark->go($route);
