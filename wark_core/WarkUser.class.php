c6b9c239d4bba596ab16172b99fe17369cdbc054
Graham Holden <opensussex@gmail.com>
2015-07-31 12:27:08 +0100

    Add sentinel methods to warkUSer class

--- wark_core/WarkUser.class.php
+++ wark_core/WarkUser.class.php
@@ -8,11 +8,11 @@ use Cartalyst\Sentinel\Native\Facades\Sentinel;
  */
 class WarkUser{
 
-    public function register(array $userVals)
+    public function register(array $credentials)
     {
          // Register a new user
         try{
-            $user = Sentinel::register($userVals);
+            $user = Sentinel::register($credentials);
             return $user;
         }catch(PDOException $e) {
             echo $e->getMessage();
@@ -20,5 +20,102 @@ class WarkUser{
         }
     }
 
+
+    public function registerAndActivate(array $credentials)
+    {
+         // Register and and Activate a user
+        try{
+            $user = Sentinel::registerAndActivate($credentials);
+            return $user;
+        }catch(PDOException $e) {
+            echo $e->getMessage();
+            return null;
+        }
+    }
+
+
+    public function authenticate(array $credentials)
+    {
+         // Register a new user
+        try{
+            $user = Sentinel::authenticate($credentials);
+            return $user;
+        }catch(PDOException $e) {
+            echo $e->getMessage();
+            return null;
+        }
+    }
+
+    public function authenticateAndRemember(array $credentials)
+    {
+         // Register a new user
+        try{
+            $user = Sentinel::authenticateAndRemember($credentials);
+            return $user;
+        }catch(PDOException $e) {
+            echo $e->getMessage();
+            return null;
+        }
+    }
+
+    public function forceAuthentication(array $credentials)
+    {
+         // Register a new user
+        try{
+            $user = Sentinel::forceAuthentication($credentials);
+            return $user;
+        }catch(PDOException $e) {
+            echo $e->getMessage();
+            return null;
+        }
+    }
+
+    public function forceAuthenticationAndRemember(array $credentials)
+    {
+         // Register a new user
+        try{
+            $user = Sentinel::forceAuthenticationAndRemember($credentials);
+            return $user;
+        }catch(PDOException $e) {
+            echo $e->getMessage();
+            return null;
+        }
+    }
+
+    public function check()
+    {
+         // Register a new user
+        try{
+            $user = Sentinel::check();
+            return $user;
+        }catch(PDOException $e) {
+            echo $e->getMessage();
+            return null;
+        }
+    }
+
+    public function guest()
+    {
+         // Register a new user
+        try{
+            $user = Sentinel::guest();
+            return $user;
+        }catch(PDOException $e) {
+            echo $e->getMessage();
+            return null;
+        }
+    }
+
+    public function getUser()
+    {
+         // Register a new user
+        try{
+            $user = Sentinel::getUser();
+            return $user;
+        }catch(PDOException $e) {
+            echo $e->getMessage();
+            return null;
+        }
+    }
 
 }        
