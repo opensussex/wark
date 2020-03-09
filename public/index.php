ba0fab5ac4ff896f1cdb8924975319c96f555961
opensussex <opensussex@gmail.com>
2020-03-09 08:46:49 +0000

    re-arrange the file structure.

--- public/index.php
+++ public/index.php
@@ -1,4 +1,4 @@
 <?php
-    include_once('config.php');
-    include_once('routes.php');
+    include_once('../src/app/config.php');
+    include_once('../src/app/routes.php');
     include_once(CORE_DIR.'boot.php');
--- src/app/config.php
+++ src/app/config.php
@@ -3,13 +3,13 @@
 ini_set('display_errors', 1);
 ini_set('error_reporting', E_ALL);
 
-define('CONTROLLER_DIR', 'controllers/');
+define('CONTROLLER_DIR', '../src/app/controllers/');
 define('CONTROLLER_SUFFIX', '.class.php');
-define('VIEW_DIR', 'views/');
+define('VIEW_DIR', '../src/app/views/');
 define('VIEW_SUFFIX', '.view.php');
-define('MODEL_DIR', 'models/');
+define('MODEL_DIR', '../src/app/models/');
 define('MODEL_SUFFIX', '.class.php');
 
-define('CORE_DIR', dirname(__FILE__) . '/../');
+define('CORE_DIR', dirname(__FILE__) . '/../../');
 define('DEPLOY_DIR', '');
 define('PRODUCTION', false);
