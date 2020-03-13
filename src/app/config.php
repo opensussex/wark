9799c57da5fbbf25c1cdf563f3a46599676d4f82
opensussex <opensussex@gmail.com>
2020-03-13 14:40:43 +0000

    alter the config

--- src/app/config.php
+++ src/app/config.php
@@ -3,12 +3,8 @@
 ini_set('display_errors', 1);
 ini_set('error_reporting', E_ALL);
 
-define('CONTROLLER_DIR', '../src/app/controllers/');
-define('CONTROLLER_SUFFIX', '.class.php');
 define('VIEW_DIR', '../src/app/views/');
 define('VIEW_SUFFIX', '.view.php');
-define('MODEL_DIR', '../src/app/models/');
-define('MODEL_SUFFIX', '.class.php');
 
 define('CORE_DIR', dirname(__FILE__) . '/../../');
 define('DEPLOY_DIR', '');
